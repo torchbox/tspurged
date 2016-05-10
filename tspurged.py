21f6ef292452cb415d90ad33434690164748a297
Felicity Tarnell <ft@le-Fay.ORG>
2016-05-10 10:13:19 +0100

    implement allow list

--- tspurged.py
+++ tspurged.py
@@ -6,15 +6,15 @@
 # 2016-05-10 ft: modified for TS
 #
 
-from flask import Flask, make_response
+from flask import Flask, request, make_response
 app = Flask(__name__)
 
 import os
 from kyotocabinet import DB
 import settings
 
-def text_response(text):
-    response = make_response(text)
+def text_response(text, code = 200):
+    response = make_response(text, code)
     response.headers['Content-Type'] = 'text/plain;charset=UTF-8'
     return response
 
@@ -24,16 +24,19 @@ def main():
 
 @app.route("/purge/<domain>/<int:genid>", methods=[ 'POST' ])
 def purge(domain, genid):
+    if request.remote_addr not in settings.ALLOW:
+        return text_response("Not permitted.\n", 403)
+
     db = DB()
 
     if not db.open(settings.GENID_DATABASE, DB.OWRITER | DB.OCREATE):
-        return text_response("Failed to purge: cannot open database.\n")
+        return text_response("Failed to purge: cannot open database.\n", 501)
 
     set_ok = db.set(domain, genid)
     db.close()
 
     if not set_ok:
-        return text_response("Failed to purge: cannot set genid.\n")
+        return text_response("Failed to purge: cannot set genid.\n", 501)
     else:
         return text_response("Purged <%s>\n" % (domain,))
     
